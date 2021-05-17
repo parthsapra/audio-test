f8c30b0985ef1936d88a65b52014aabf58395c0e
parthsapra <parthsapra91@gmail.com>
2021-05-17 13:15:40 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { Howl } from 'howler';
 
 @Component({
@@ -6,14 +6,18 @@ import { Howl } from 'howler';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = ',This is audio test';
   sound: any = new Howl({
-    src: ['../assets/krishna-flute.mp3'],
-    // src: ['../assets/sound.mp3'],
+    src: [
+      'https://cdn.jsdelivr.net/gh/parthsapra/audio-test@master/src/Krrish-Flute-Ringtone.mp3'
+    ],
     loop: true
   });
-
+  durationOfSOund: any;
+  ngOnInit() {
+    this.durationOfSOund = this.sound.duration();
+  }
   playSound() {
     this.sound.play();
   }
